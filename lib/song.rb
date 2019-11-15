967bba06b6fda539f291185fe16ad3175e8504af
connor worthen <connorw220@protonmail.com>
2019-11-15 21:48:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,11 @@ class Song
     self.class.all << self
   end
 
-  def create(song)
-  song = Song.create
-  Song.all.include?(song)
-end
+  def self.create(name)
+    song = self.new
+    song.name = name
+    @@all << song
+  end
+
 
 end
